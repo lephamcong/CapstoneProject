852380e2ca74d6890b9bd248d431b2d039c7c626
Huynh Nhat Anh <anhhuynh09092002@gmail.com>
2025-04-25 14:01:22 +0700

    Update tree folder

--- Simulation/run.sh
+++ Simulation/run.sh
@@ -1,13 +1,39 @@
-set -e
+#!/bin/bash
 
-echo "Compiling UESimulation"
-gcc -o UESimulation UESimulation.c
+pkill server
+pkill client
 
-echo "Compiling Scheduler"
-gcc -o Scheduler Scheduler.c roundrobin.c
+SCENARIORS=("high_traffic" "low_traffic" "burst_traffic" "ideal_condition" "mobility_pattern" "ideal_condition_bsr10000" "ideal_condition_bsr100000")
+TYPE_SCHEDULER=("rr" "max_cqi" "pf")
 
-echo "Running Scheduler"
-./Scheduler
+gcc -o clean clean.c
+gcc -o server server.c define.h
+gcc -o client client.c define.h
+mkdir -p results
 
-echo "Running UESimulation"
-./UESimulation
+for SCENARIO in "${SCENARIORS[@]}"; do
+    # Compile the server.c file
+
+    mkdir -p "results/$SCENARIO"
+
+    for TYPE in "${TYPE_SCHEDULER[@]}"; do
+        ./clean
+        echo "Running server with scheduler type: $TYPE with scenario: $SCENARIO"
+        ./server "results/$SCENARIO/$TYPE.csv" $TYPE &
+        ./client "gen_cqi/cqi_$SCENARIO.csv" "gen_cqi/bsr_$SCENARIO.csv" 
+        wait
+        echo "Complete: $SCENARIO with scheduler type: $TYPE"
+    done
+done
+
+echo "Done."
+
+
+# if (strcmp(type_scheduler,"rr") == 0) scheduler_type = ROUND_ROBIN; // Default scheduler type
+#     else if (strcmp(type_scheduler,"max_cqi") == 0) scheduler_type = MAX_CQI;
+#     else if (strcmp(type_scheduler,"pf") == 0) scheduler_type = PROPORTIONAL_FAIR;
+#     else if (strcmp(type_scheduler,"q_learning") == 0) scheduler_type = Q_LEARNING;
+#     else {
+#         LOG_ERROR("Invalid scheduler type");
+#         return 1;
+#     }
